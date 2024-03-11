f65c10d25119cbc5e854f8b99eb3004b1e04c4e0
quackextractor <miro.slezak0601@gmail.com>
2024-03-11 08:52:39 +0100

    changed enum PlantTypes.java to only contain flowers, initialized variables in the Farm.java class

--- src/Farm.java
+++ src/Farm.java
@@ -1,3 +1,37 @@
-public class Farm {
+import java.util.ArrayList;
 
-}
+    public class Farm {
+    private int flowerTypeMaxAmount = 5;
+    private ArrayList<Flower> flowers;
+    private int size;
+
+    public Farm(int flowerTypeMaxAmount, ArrayList<Flower> flowers, int size) {
+        this.flowerTypeMaxAmount = flowerTypeMaxAmount;
+        this.flowers = flowers;
+        this.size = size;
+    }
+
+        public int getFlowerTypeMaxAmount() {
+            return flowerTypeMaxAmount;
+        }
+
+        public void setFlowerTypeMaxAmount(int flowerTypeMaxAmount) {
+            this.flowerTypeMaxAmount = flowerTypeMaxAmount;
+        }
+
+        public ArrayList<Flower> getFlowers() {
+            return flowers;
+        }
+
+        public void setFlowers(ArrayList<Flower> flowers) {
+            this.flowers = flowers;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public void setSize(int size) {
+            this.size = size;
+        }
+    }
