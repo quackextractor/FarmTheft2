1c88548b11b614a7fb796ec5cdad49a4f90fc54c
quackextractor <miro.slezak0601@gmail.com>
2024-03-11 09:04:00 +0100

    added plantFlower() method in Farm.java

--- src/Farm.java
+++ src/Farm.java
@@ -1,37 +1,49 @@
 import java.util.ArrayList;
+import java.util.concurrent.Flow;
 
-    public class Farm {
+public class Farm {
     private int flowerTypeMaxAmount = 5;
     private ArrayList<Flower> flowers;
-    private int size;
+    private int haSize;
 
-    public Farm(int flowerTypeMaxAmount, ArrayList<Flower> flowers, int size) {
+    public Farm(int flowerTypeMaxAmount, int haSize) {
         this.flowerTypeMaxAmount = flowerTypeMaxAmount;
+        this.haSize = 100;
+    }
+
+    public void plantFlower(Flower flower) {
+        if ((haSize - flower.neededArea) <= 0) {
+            throw new UnsupportedOperationException("Not enough space available to commit to planting.");
+        } else {
+            haSize -= flower.neededArea;
+            flowers.add(flower);
+        }
+    }
+
+    public void waterFlower() {
+    }
+
+    public int getFlowerTypeMaxAmount() {
+        return flowerTypeMaxAmount;
+    }
+
+    public void setFlowerTypeMaxAmount(int flowerTypeMaxAmount) {
+        this.flowerTypeMaxAmount = flowerTypeMaxAmount;
+    }
+
+    public ArrayList<Flower> getFlowers() {
+        return flowers;
+    }
+
+    public void setFlowers(ArrayList<Flower> flowers) {
         this.flowers = flowers;
-        this.size = size;
     }
 
-        public int getFlowerTypeMaxAmount() {
-            return flowerTypeMaxAmount;
-        }
-
-        public void setFlowerTypeMaxAmount(int flowerTypeMaxAmount) {
-            this.flowerTypeMaxAmount = flowerTypeMaxAmount;
-        }
-
-        public ArrayList<Flower> getFlowers() {
-            return flowers;
-        }
-
-        public void setFlowers(ArrayList<Flower> flowers) {
-            this.flowers = flowers;
-        }
-
-        public int getSize() {
-            return size;
-        }
-
-        public void setSize(int size) {
-            this.size = size;
-        }
+    public int getHaSize() {
+        return haSize;
     }
+
+    public void setHaSize(int haSize) {
+        this.haSize = haSize;
+    }
+}
